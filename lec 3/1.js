40f3e578da1182039d56f8fe78fdce3a8a082cd9
puneetsharma07029-cyber <puneetsharma07029@gmail.com>
2026-01-25 20:49:26 +0530

    notes

---
+++ lec 3/1.js
@@ -0,0 +1,26 @@
+// let str = "tu meri me tera me teri tu mera";
+// let words = str.split("r").length-1;
+// console.log(words)
+
+// Regular function
+
+function demo(){
+    console.log("demo")
+}
+demo()
+
+
+function sample(a,b){
+    return a+b;
+
+}
+console.log(sample(5,10))
+
+// Arrow function
+// advantages: doesn't need return and function keyword and it will perform asynchronously
+
+const test=(c,d)=> c+d;
+// if we have multiple statements then we have to add return
+
+console.log(test(4,6))
+
---
+++ lec 4/2.js
@@ -0,0 +1,14 @@
+let user={
+    FullName:"Puneet Sharma",
+    Address:"Delhi, India",
+    Mobileno:"9876543210",
+    Favcolor:["red","blue","green"],
+}
+console.log(user);
+
+
+
+let date=new Date();
+console.log(date);
+
+
