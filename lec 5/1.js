780e6594e722a40fa8fdafba0baf7dc1826abf3b
puneetsharma07029-cyber <puneetsharma07029@gmail.com>
2026-01-27 12:35:14 +0530

    notes

---
+++ lec 5/1.js
@@ -0,0 +1,17 @@
+// var a = 10
+// console.log(a);
+// function demo(){
+//     let b = 20
+//     console.log(b);
+// }
+// demo();
+
+let user={
+    fullname:"Puneet Sharma",
+    address:"gurugram",
+    mobileno:86876768568,
+    favcolor:["blue","red"]
+
+}
+console.log(user.favcolor[1]);
+console.log(object.keys(user))
