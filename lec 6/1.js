2afd33e954d1cdd36e554596ded59ed103c2122b
puneetsharma07029-cyber <puneetsharma07029@gmail.com>
2026-01-28 17:21:15 +0530

    notes

---
+++ lec 6/1.js
@@ -0,0 +1,15 @@
+// const headingElement =document.getElementById('myHeading');
+// console.log(headingElement);
+
+// headingElement.textContent = "new Heading Text";
+// headingElement.style.color = "blue";
+// headingElement.style.backgroundColor ="yellow";
+
+
+const paragraph = document.querySelector("#content 1");
+paragraph.textContent ="this paragraph has been updated using querySelector";
+paragraph.Style.color="blue";
+const heading = document.querySelector("#content h1");
+heading.textContent = "Hello, DOM!";
+const title = document.querySelector("title");
+title.textContent = "Updated Title";
